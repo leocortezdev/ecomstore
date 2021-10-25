534c21c16fff6e1b671488da0f640a5851743402
LCFirm <leo.cortez55@gmail.com>
2021-10-25 16:36:07 -0400

    Register Page finished

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Home from './pages/Home';
 import './App.css';
 import ProductList from './pages/ProductList';
 import SingleProduct from './pages/SingleProduct';
+import Register from './pages/Register';
 
 const App = () => {
    return (
-       <SingleProduct />
+       <Register />
    )
 }
 
